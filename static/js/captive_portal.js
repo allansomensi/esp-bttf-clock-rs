5303546f8d12c4d48651a0db1e8618652c20fa56
allansomensi <allansomensidev@proton.me>
2025-03-24 12:14:34 -0300

    style(view): :lipstick: improve UI for `captive_portal` and `web_portal`

--- static/js/captive_portal.js
+++ static/js/captive_portal.js
@@ -1,20 +1,20 @@
 function connectWiFi() {
-    let ssid = document.getElementById('ssidInput').value;
-    let password = document.getElementById('passwordInput').value;
+    let ssid = document.getElementById("ssidInput").value;
+    let password = document.getElementById("passwordInput").value;
 
     if (ssid && password) {
-        fetch('/set_config', {
-            method: 'POST',
+        fetch("/set_config", {
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json'
+                "Content-Type": "application/json",
             },
-            body: JSON.stringify({ ssid: ssid, password: password })
+            body: JSON.stringify({ ssid: ssid, password: password }),
         })
-            .then(response => response.json())
-            .then(data => {
-                document.getElementById('message').innerText = data.message;
+            .then((response) => response.json())
+            .then((data) => {
+                document.getElementById("message").innerText = data.message;
             })
-            .catch(error => console.error('Error:', error));
+            .catch((error) => console.error("Error:", error));
     } else {
         alert("Please fill in both SSID and password.");
     }
